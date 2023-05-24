5e524d670eeef08f51041db489efbd3dc3181cf0
Nikita <nikturchinovich@mail.ru>
2023-05-24 21:07:59 +0300

    Initial commit

---
+++ yos.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name [YOS] - Open Spotify from YouTube
+// @description A small extension that adds a button to open Spotify app search from youtube video
+// @version 0.0.1
+// @match *://*.youtube.com/watch*
+// @author fy5tew
+// @homepageURL https://github.com/fy5tew/youtube-open-spotify
+// @downloadURL https://raw.githubusercontent.com/fy5tew/youtube-open-spotify/master/yos.user.js
+// @updateURL https://raw.githubusercontent.com/fy5tew/youtube-open-spotify/master/yos.user.js
+// @supportURL https://github.com/fy5tew/youtube-open-spotify/issues
+// @namespace yos
+// ==/UserScript==
+
+(() => {
+    "use strict";
+
+    const log = (message) => console.log(`[YOS] ${message}`);
+
+    log("extension started");
+})();
